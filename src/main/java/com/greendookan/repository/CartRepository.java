e9f3c5bae39b71e50b6d3ddfd36e7014a6b6cae6
aditianand008 <aaditianand8@gmail.com>
2025-11-08 00:59:27 +0530

    Create CartRepository interface for cart management

---
+++ src/main/java/com/greendookan/repository/CartRepository.java
@@ -0,0 +1,17 @@
+package com.greendookan.repository;
+
+import com.greendookan.model.Cart;
+import com.greendookan.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    Optional<Cart> findByUser(User user);
+
+    Optional<Cart> findByUserId(Long userId);
+
+    void deleteByUser(User user);
+}
